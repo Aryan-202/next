511d7b76e630f889f0ab2a2fe069ee7f35c94a79
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-27 15:26:08 +0530

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Homepage() {
   return (
     <div>
-      <h1>Welcome to the Homepage</h1>
+      <h1 className="text-4xl font-bold bg-amber-50">Welcome to the Homepage</h1>
     </div>
   )
 }
